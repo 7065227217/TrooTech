9802b44f21c8bec9294ae2c7cc6648084d3b5e5a
root <vsaini12381@gmail.com>
2021-05-27 23:46:22 +0530

    commit

--- db/web_task.sql
+++ db/web_task.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: May 25, 2021 at 07:04 PM
+-- Generation Time: May 27, 2021 at 08:15 PM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.4.11
 
@@ -20,6 +20,25 @@ SET time_zone = "+00:00";
 --
 -- Database: `web_task`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `password`, `email`) VALUES
+(1, '12345678', 'admin@gmail.com');
 
 -- --------------------------------------------------------
 
@@ -43,7 +62,7 @@ CREATE TABLE `product` (
 --
 
 INSERT INTO `product` (`product_id`, `category_id`, `sub_category_id`, `name`, `price`, `created_time`, `updated_time`, `status`) VALUES
-(1, 1, 12, 'firstwww444', 2002, '2021-05-25 22:17:18', '2021-05-25 22:19:06', 1);
+(1, 1, 7, 'First Product', 1000, '2021-05-27 23:37:59', '2021-05-27 23:37:59', 1);
 
 -- --------------------------------------------------------
 
@@ -64,28 +83,27 @@ CREATE TABLE `product_category` (
 --
 
 INSERT INTO `product_category` (`category_id`, `category_parent_id`, `name`, `status`, `created_time`) VALUES
-(1, 0, 'Level 1', 1, '2021-05-25 22:11:08'),
-(2, 0, 'Level 2', 1, '2021-05-25 22:11:15'),
-(3, 0, 'Level 3', 1, '2021-05-25 22:11:19'),
-(4, 1, 'Level 1 Child 1', 1, '2021-05-25 22:12:16'),
-(5, 1, 'Level 1 Child 2', 1, '2021-05-25 22:12:19'),
-(6, 1, 'Level 1 Child 3', 1, '2021-05-25 22:12:23'),
-(7, 2, 'Level 2 Child 1', 1, '2021-05-25 22:12:31'),
-(8, 2, 'Level 2 Child 2', 1, '2021-05-25 22:12:34'),
-(9, 3, 'Level 3 Child 1', 1, '2021-05-25 22:12:45'),
-(10, 3, 'Level 3 Child 2', 1, '2021-05-25 22:12:48'),
-(11, 3, 'Level 3 Child 3', 1, '2021-05-25 22:12:51'),
-(12, 4, 'Level 1 Child 1 SubChlid 1', 1, '2021-05-25 22:13:44'),
-(13, 4, 'Level 1 Child 1 SubChlid 2', 1, '2021-05-25 22:13:57'),
-(14, 4, 'Level 1 Child 1 SubChlid 3', 1, '2021-05-25 22:14:00'),
-(15, 12, 'Level 1 Child 1 SubChlid 1 SubSubChild 1', 1, '2021-05-25 22:14:43'),
-(16, 12, 'Level 1 Child 1 SubChlid 1 SubSubChild 2', 1, '2021-05-25 22:14:46'),
-(17, 12, 'Level 1 Child 1 SubChlid 1 SubSubChild 3', 1, '2021-05-25 22:14:50'),
-(18, 0, 'level 4', 1, '2021-05-25 22:23:39');
+(1, 0, 'Level 1', 1, '2021-05-27 23:32:35'),
+(2, 0, 'Level 2', 1, '2021-05-27 23:32:41'),
+(3, 0, 'Level 3', 1, '2021-05-27 23:32:45'),
+(4, 1, 'Level 1-1', 1, '2021-05-27 23:32:53'),
+(6, 4, 'Level 1-2', 1, '2021-05-27 23:33:42'),
+(7, 6, 'Level 1-3', 1, '2021-05-27 23:34:01'),
+(8, 1, 'Level 1-2', 1, '2021-05-27 23:36:40'),
+(9, 0, 'Level 4', 1, '2021-05-27 23:36:50'),
+(10, 9, 'Level 4-1', 1, '2021-05-27 23:37:01'),
+(11, 9, 'Level 4-2', 1, '2021-05-27 23:37:13'),
+(12, 10, 'Level 4-1-1', 1, '2021-05-27 23:37:29');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `product`
@@ -104,6 +122,12 @@ ALTER TABLE `product_category`
 --
 
 --
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
 -- AUTO_INCREMENT for table `product`
 --
 ALTER TABLE `product`
@@ -113,7 +137,7 @@ ALTER TABLE `product`
 -- AUTO_INCREMENT for table `product_category`
 --
 ALTER TABLE `product_category`
-  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
